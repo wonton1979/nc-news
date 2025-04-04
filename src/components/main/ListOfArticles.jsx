9b7aa44ce5baaef3743bddb4fac727e8b6b0f3bc
Yejun <guanyejun79@gmail.com>
2025-04-04 09:15:52 +0100

    small UI problem fixed

--- src/components/main/ListOfArticles.jsx
+++ src/components/main/ListOfArticles.jsx
@@ -228,6 +228,18 @@ export default function ListOfArticles() {
                         </button>
                     </div>
 
+                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+                        focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-7 py-2 ml-28 text-center
+                         dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:hidden mt-5" onClick={handleFilterSearch}>
+                        Search Articles
+                    </button>
+
+                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4
+                        focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-7 py-2 ml-5 text-center
+                         dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:hidden mt-5" onClick={()=>{window.location.reload()}}>
+                        Reset Filters
+                    </button>
+
                 </div>
 
                 <div
